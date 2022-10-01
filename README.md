dc18eba734de94f3adb72f3b4f17596f7e7415f0
ふぁ <34892635+fa0311@users.noreply.github.com>
2022-10-01 19:10:18 +0900

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# youtube-alert-blocker
+
+[youtube.com](https://youtube.com.com/) の 警告を自動で続行する拡張機能です<br>
+
+これのこと
+![screenshot](docs\img\screenshot.png)
+
+# インストール
+
+[releases](https://github.com/fa0311/youtube-alert-blocker/releases) の manifest_v3.zip を選択してファイルをダウンロード
+Chrome の 拡張機能の管理 を選択して デベロッパーモード を有効にした後、ダウンロードした zip ファイルをページにドラッグしてください
+
+# License
+
+youtube-alert-blocker is under MIT License
---
+++ src/ts/main.ts
@@ -0,0 +1,21 @@
+const bypasser = (nodeList: HTMLElement) => {
+    const query = 'div#player-error-message-container tp-yt-paper-button#button';
+    if(nodeList.parentElement == null) return;
+    const feedList:  NodeListOf<Element> = nodeList.parentElement.querySelectorAll(query);
+    for (const feed of feedList) {
+        (<HTMLElement>feed).click();
+        console.log("[choge-blog-bypass-ad-blocking-detection] success.");
+    }
+}
+
+const observer = new MutationObserver(records => {
+    for (const record of records) {
+        for (const nodeList of record.addedNodes) {
+            if (nodeList instanceof HTMLElement) {
+                bypasser(nodeList);
+            }
+        }
+    }
+});
+
+observer.observe(document.documentElement, { childList: true, subtree: true });
