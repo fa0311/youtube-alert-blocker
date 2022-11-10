25b3a69170eb57ef72456f7ebc838381ce94b683
ふぁ <yuki@yuki0311.com>
2022-11-10 14:21:57 +0900

    update format
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,20 +1,20 @@
 const bypasser = (nodeList: HTMLElement) => {
-    const query = 'div#player-error-message-container tp-yt-paper-button#button';
-    const feedList:  NodeListOf<Element> = nodeList.querySelectorAll(query);
-    for (const feed of feedList) {
-        (<HTMLElement>feed).click();
-        console.log("[youtube-alert-blocker] success.");
-    }
-}
+  const query = "div#player-error-message-container tp-yt-paper-button#button";
+  const feedList: NodeListOf<Element> = nodeList.querySelectorAll(query);
+  for (const feed of feedList) {
+    (<HTMLElement>feed).click();
+    console.log("[youtube-alert-blocker] success.");
+  }
+};
 
-const observer = new MutationObserver(records => {
-    for (const record of records) {
-        for (const nodeList of record.addedNodes) {
-            if (nodeList instanceof HTMLElement) {
-                bypasser(nodeList);
-            }
-        }
+const observer = new MutationObserver((records) => {
+  for (const record of records) {
+    for (const nodeList of record.addedNodes) {
+      if (nodeList instanceof HTMLElement) {
+        bypasser(nodeList);
+      }
     }
+  }
 });
 
 observer.observe(document.documentElement, { childList: true, subtree: true });
